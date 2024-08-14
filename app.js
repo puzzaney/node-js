22ca8b28183e81708d7e0cb3cfae0554bab3bb57
puzzaney <poojankarki12@gmail.com>
2024-08-14 22:22:45 +0545

    Sent a simple HTML  response

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 function rqListener(req, res) {
-  console.log(req);
-  process.exit();
+  console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+  res.write('</htm>');
+  res.end();
 }
 
 const server = http.createServer(rqListener);
